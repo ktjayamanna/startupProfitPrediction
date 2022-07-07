b3ffb71aaf8782c00fa9471ad7a15a23d0b84154
Kaveen Jayamanna <kaveen.jayamanna@gmail.com>
2022-07-06 23:29:34 -0500

    connected to a frontend

---
+++ backEnd.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import pickle
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+import os
+
+app = Flask(__name__)
+pickle_input = open("startup_50.pkl", "rb")
+model = pickle.load(pickle_input)
+
+
+def normalize_feature(featureIn, featureIdxIn):
+    feature = float(featureIn)
+    maxVals = [165349.2, 182645.56, 471784.1]
+    if feature > maxVals[featureIdxIn]:
+        return 1
+    else:
+        return feature / maxVals[featureIdxIn]
+
+
+# homepage
+@app.route("/")
+def homePage():
+    return render_template("home.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    int_features = [x for x in request.form.values()]
+    if int_features[-1] == "Other":
+        return render_template(
+            "home.html",
+            pred="Sorry!, we do not cover your area yet, please check again in the future!",
+        )
+    else:
+        print(int_features)
+        if int_features[-1] == "New York":
+            int_features[-1] = 0
+            int_features.append(1)
+        elif int_features[-1] == "Florida":
+            int_features[-1] = 1
+            int_features.append(0)
+        else:
+            int_features[-1] = 0
+            int_features.append(0)
+        for i in range(3):
+            int_features[i] = normalize_feature(int_features[i], i)
+        prediction = model.predict([int_features])
+        prediction = prediction[0]
+        return render_template(
+            "home.html",
+            pred="The profit of your company is ${}".format(round(prediction[0], 2)),
+        )
+
+
+# run the app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8010)
--- fiftyStartups.py
+++ fiftyStartups.py
@@ -5,56 +5,64 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import seaborn as sns
-plt.close('all')
+import pickle
+
+plt.close("all")
 #%% import the dataset
 df = pd.read_csv("./50_Startups.csv")
 #%% Seperate independent variables and the dependent variable.
-x = df.iloc[:, : -1]
-y = df.iloc[:, -1 :]
+x = df.iloc[:, :-1]
+y = df.iloc[:, -1:]
 #%% Check the correlation between dependent variables and ind. variables.
 plt.figure
-sns.pairplot(df, x_vars= x, y_vars= y, hue = "State")
-plt.savefig('./plots/correlation.png')
+sns.pairplot(df, x_vars=x, y_vars=y, hue="State")
+plt.savefig("./plots/correlation.png")
 
-#%%checkout the multicollinearity 
+#%%checkout the multicollinearity
 plt.figure
 sns.pairplot(x.iloc[:, :3])
-plt.savefig('./plots/multicollinearity.png')
+plt.savefig("./plots/multicollinearity.png")
 
 
 #%% check the peak to peak values of the features
-'''
+"""
 We have numerical features for the most part except "state" which is nominal.
 So, we have to one hot encode this. Remember that we only need two digits to 
 represent ['New York', 'California', 'Florida'].
-'''
-state = pd.get_dummies(x['State'], drop_first = True)
-#Now we drop the existing state variable and add the newly created back
-x = x.drop('State', axis = 1)
-x = pd.concat([x, state], axis = 1)
+"""
+state = pd.get_dummies(x["State"], drop_first=True)
+# Now we drop the existing state variable and add the newly created back
+x = x.drop("State", axis=1)
+x = pd.concat([x, state], axis=1)
 #%% check the peak to peak values of each feature
 cols = x.columns
+
+
 def get_range(seriesIn):
     maxVal = seriesIn.max()
     minVal = seriesIn.min()
     return maxVal - minVal
+
+
 peak2peak = [get_range(x[i]) for i in cols]
 #%%
 def normalize_feature(featureLabelIn):
     maxVal = x[featureLabelIn].max()
+    print(f"{featureLabelIn} Max Value is", maxVal)
     x[featureLabelIn] = x[featureLabelIn] / maxVal
+
+
 for i in range(3):
     normalize_feature(cols[i])
 #%% Split into train and test
-x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.3, 
-                                                    random_state=5)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=5)
 #%%
 model = LinearRegression()
 model.fit(x_train, y_train)
 optimalW = model.coef_
 optimalB = model.intercept_
 #%% Make predictions and calcuate the r2 score
-yPred= model.predict(x_test)
+yPred = model.predict(x_test)
 r2Score = r2_score(y_test, yPred)
 print(f"R^2 score is {round(r2Score * 100, 2)}%")
 #%%
@@ -62,24 +70,31 @@ def plot_features(FeatureIdxIn):
     selectedFeature = cols[FeatureIdxIn]
     plt.figure()
     # Plot the linear fit
-    plt.plot(x_train[selectedFeature],(optimalW[0][FeatureIdxIn] * x_train[selectedFeature] + optimalB ),
-             c = "g",label = f'Fitted line for {selectedFeature}')
-    plt.scatter(x_test[selectedFeature], yPred, c = "b", label = "Model Predictions")
-    # Create a scatter plot of the data. 
-    plt.scatter(x_test[selectedFeature], y_test, marker='x', c='r', label = "Target Labels") 
+    plt.plot(
+        x_train[selectedFeature],
+        (optimalW[0][FeatureIdxIn] * x_train[selectedFeature] + optimalB),
+        c="g",
+        label=f"Fitted line for {selectedFeature}",
+    )
+    plt.scatter(x_test[selectedFeature], yPred, c="b", label="Model Predictions")
+    # Create a scatter plot of the data.
+    plt.scatter(
+        x_test[selectedFeature], y_test, marker="x", c="r", label="Target Labels"
+    )
     # Set the title
     plt.title(f"Startup Return of Investment with Respect to {selectedFeature}")
     # Set the y-axis label
-    plt.ylabel('profits in USD')
+    plt.ylabel("profits in USD")
     # Set the x-axis label
     plt.xlabel(selectedFeature)
-    #Set the legend
+    # Set the legend
     plt.legend()
-    plt.savefig(f'./plots/{selectedFeature}.png')
+    plt.savefig(f"./plots/{selectedFeature}.png")
+
 
 for featureIdx in range(len(cols[0:3])):
     plot_features(featureIdx)
-
-
-
-
+#%% serialize the model
+pickle_output = open("startup_50.pkl", "wb")
+pickle.dump(model, pickle_output)
+pickle_output.close()
